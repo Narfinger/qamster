07011eeea70b1ef6105087d17ea2372a71b35d0b
Narfinger <Narfinger@users.noreply.github.com>
2014-12-29 19:33:51 +0100

    use subclasses because I want to have different type of plots but the same style

--- src/category-histogram.h
+++ src/category-histogram.h
@@ -1,44 +1,41 @@
 /*
  * <one line to give the program's name and a brief idea of what it does.>
  * Copyright (C) 2014  Narfinger
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 
-#ifndef HISTOGRAM_H
-#define HISTOGRAM_H
+#ifndef CATEGORYHISTOGRAM_H
+#define CATEGORYHISTOGRAM_H
 
-#include <QSqlDatabase>
+#include "histogram.h"
 
-#include "../lib/qcustomplot.h"
-
-class Histogram : public QCustomPlot
+class CategoryHistogram : public Histogram
 {
     Q_OBJECT
 public:
-  Histogram(QWidget* parent = 0) : QCustomPlot(parent) {};
+  CategoryHistogram(QWidget* parent = 0);
 
-  void setDb(QSqlDatabase db) { db_ = db; this->setupCategoryHistogram(); };
-  virtual void drawWeek(const QDate& start, const QDate& end) = 0;
+  virtual void drawWeek(const QDate& start, const QDate& end);
+
+protected:
+  virtual void setupCategoryHistogram();      //this can only be called if we set a db and not in the constructor
   
-protected :
-  virtual void setupCategoryHistogram() = 0;        //this can only be called if we set a db and not in the constructor
-  void initialSetupHistogram();
 private:
   QSqlDatabase db_;
-  QVector<QCPBars*> bars_;      //the widget is responsible for destruction
+  QVector<QCPBars*> bars_;	//the widget is responsible for destruction
   QVector<double> ticks_;
 };
 
