f3a388a09d3da7a68f0916ab8d922e113bdcc2bc
Narfinger <Narfinger@users.noreply.github.com>
2016-03-03 21:11:29 +0900

    now we use angularjs

--- go-appengine/main.go
+++ go-appengine/main.go
@@ -3,51 +3,27 @@ package qamster
 import (
         "net/http"
 //        "time"
-	"html/template"
-	"fmt"
         //"appengine"
+	"html/template"
         // "appengine/datastore"
         // "appengine/user"
 )
 
-const bodyTemplateString = `<html>
-<head><title>TTTT</title></head>
-<body>
-this is from root</br>
-{{template "body" .}}
-</body>
-</html>`
-
-const rootTemplateString = `{{define "body"}}
-<p><b>TTTTT</p></b>
-{{end}}`
-
-
-
 func init() {	
-        http.HandleFunc("/", root)
+        //http.HandleFunc("/", root)
 }
 
 // [START func_root]
 func root(w http.ResponseWriter, r *http.Request) {
-	
-	bodyTemplate, err := template.New("body").Parse(bodyTemplateString)
-	if err!= nil {
-		http.Error(w, "first: " + err.Error(), http.StatusInternalServerError)
-		fmt.Print("dat error")
+	t, err := template.ParseFiles("templates/main.html")
+	if err != nil {
+		http.Error(w,err.Error(), http.StatusInternalServerError)
 		return
 	}
-	rootTemplate, err := template.Must(bodyTemplate.Clone()).Parse(rootTemplateString)
-	if err!= nil {
-		http.Error(w, "snd: " + err.Error(), http.StatusInternalServerError)
-		fmt.Print("dat error2")
-		return
-	}
-
 	// c := appengine.NewContext(r)
-	if err := rootTemplate.Execute(w, ""); err != nil {
+	if err := t.Execute(w, "root"); err != nil {
                 http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
+	}	
 }
 // [END func_root]
