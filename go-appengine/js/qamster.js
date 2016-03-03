b4cd5582ed355c28197df6202aca969e0f6ada7f
Narfinger <Narfinger@users.noreply.github.com>
2016-03-03 22:24:11 +0900

    a bunch more changes to this actually works

--- go-appengine/js/qamster.js
+++ go-appengine/js/qamster.js
@@ -1,8 +1,22 @@
-angular.module('qamsterApp', []).controller('qamsterCtrl', function($scope) {
+var app = angular.module('qamsterApp', ['ngMaterial']);
+
+// themes
+app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('green');
+});
+
+
+app.controller('QamsterCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
     $scope.tasks = [
         {start: '12:00', end: '12:15', title:'test1', category:'work', runtime:'15min'},
         {start: '12:15', end: '12:30', title:'test2', category:'break', runtime:'15min'},
         {start: '12:30', end: '13:00', title:'test3', category:'work', runtime:'30min'}
     ];
-});
+
+}]);
 
