0d2b6e2b58fe05292ccf733c0051c18bc7c10e3e
Narfinger <Narfinger@users.noreply.github.com>
2016-05-08 21:39:37 +0900

    filter now works correctly

--- go-appengine/datastore.go
+++ go-appengine/datastore.go
@@ -1,7 +1,8 @@
 package qamster
 
 import (
-	"net/http"	
+	"net/http"
+	"time"
 	"appengine"
 	"appengine/datastore"
 )
@@ -15,6 +16,7 @@ func tasksKey(c appengine.Context) *datastore.Key {
         return datastore.NewKey(c, "Task", "default_task", 0, nil)
 }
 
+//return running task
 func ds_getRunning(r *http.Request) (bool, Task) {
 	c := appengine.NewContext(r)
 	var isRunning = IsRunningStruct{} 
@@ -25,6 +27,7 @@ func ds_getRunning(r *http.Request) (bool, Task) {
 	return isRunning.IsRunningField, isRunning.RunningTask
 }
 
+//set the running task
 func ds_setRunning(isRunning bool, rTask Task, r *http.Request) {
 	c := appengine.NewContext(r)
 	var rs = IsRunningStruct{IsRunningField: isRunning, RunningTask: rTask}
@@ -32,14 +35,24 @@ func ds_setRunning(isRunning bool, rTask Task, r *http.Request) {
 	datastore.Put(c,key, &rs)
 }
 
+//append a task
 func ds_appendTask(t Task, r *http.Request) {
 	c := appengine.NewContext(r)
 	key := datastore.NewIncompleteKey(c, "Tasks", tasksKey(c))
 	datastore.Put(c, key, &t)
 }
 
+// returns the current date with 0 hours, 0 minutes, 0 seconds and 0 milliseconds
+func getCurrentDate() (time.Time) {
+	var year,month,day = time.Now().Date()
+	var t = time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//get all current tasks and put them into t
 func ds_getTasks(t *[]Task, r *http.Request) {
 	c := appengine.NewContext(r)
-	q := datastore.NewQuery("Tasks").Order("End")
+	var today = getCurrentDate()
+	q := datastore.NewQuery("Tasks").Order("End").Filter("End >=", today)
 	q.GetAll(c, t);
 }
