fef67efa024c3bc5531b721dc07fa1b7fc7d9d7f
Narfinger <Narfinger@users.noreply.github.com>
2017-06-02 23:01:18 +0900

    some stuff is now working

--- go-appengine/static/index.jsx
+++ go-appengine/static/index.jsx
@@ -2,7 +2,7 @@
 require('./qamster.css');
 
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button, FormControl } from 'react-bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
@@ -10,50 +10,97 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 const testobj = [{id: "1", start:"2017-05-18T09:53:39.782589Z",end:"2017-05-18T09:53:44.269315Z",title:"",category:""},
 		 {id: "2", start:"2017-05-18T09:53:44.274074Z",end:"2017-05-18T09:53:53.625158Z",title:"",category:""}];
 
+class InputBar extends React.Component {
+    render() {
+	return (
+	    <div>
+		<Col md={1}></Col>
+		<Col md={4}>
+		    <FormControl></FormControl>
+		</Col>
+		<Col md={1}>
+		    <Button bsStyle="success">
+			Add Task
+		    </Button>
+		</Col><Col md={1}></Col>
+		<Col md={1}>
+    		    <Button bsStyle="danger">
+			Stop
+		    </Button>
+		</Col>
+	    </div>
+	)}
+}
+
 class TopBar extends React.Component {
     render() {
 	return (
-	    <h1>TopBar</h1>
+	    <div>
+		<Row>
+		    <h1>TopBar</h1>
+		</Row>
+		<Row>
+		    <InputBar />
+		</Row>
+	    </div>
 	)}
+}
+
+function myFormatTime(t) {
+    var d = new Date(t);
+    return d.getHours() + ":" + d.getMinutes();
+}
+
+function prepareTime(currentValue, index) {
+    return {
+	"id": index+1,
+	"start": myFormatTime(currentValue.start),
+	"end": myFormatTime(currentValue.end),
+	"title": currentValue.title,
+	"category": currentValue.category,
+    }
 }
 
 class MainSite extends React.Component{
     constructor(props) {
 	super(props);
-	this.state = {
-	    times: []
-	};
+	this.state = {times: []};
     }
 
     componentDidMount() {
-	console.log("called");
 	var req = new Request("/go/timetable");
 	fetch("/go/timetable")
 	    .then((responseText) => responseText.json())
-	    .then((response) => this.setState(response));
+	    .then((response) => this.setState({times: response.map(prepareTime)}));
     }
 
     render() {
 	return (
-	    <Row>
-		<Col md={1}></Col>
-		<Col md={10}>
-		    <div>
-			<Row>
-			    <TopBar />
-			</Row>
-			<Row>
-			    <BootstrapTable data={this.state.times} striped hover>
-				<TableHeaderColumn isKey dataField='id'>Id</TableHeaderColumn>
-				<TableHeaderColumn dataField='start'>Start</TableHeaderColumn>
-				<TableHeaderColumn dataField='end'>End</TableHeaderColumn>
-				<TableHeaderColumn dataField='title'>Title</TableHeaderColumn>
-				<TableHeaderColumn dataField='category'>Category</TableHeaderColumn>
-			    </BootstrapTable>
-			</Row>
-		    </div>
-		</Col>
-	    </Row>
+	    <div>
+		<Row>
+		    <h1> THIS IS NOT SECURE, SECURE APP.YAML
+		    </h1>
+		</Row>
+		<Row>
+		    <Col md={1}></Col>
+		    <Col md={10}>
+			<div>
+			    <Row>
+				<TopBar />
+			    </Row>
+			    <Row>
+				<BootstrapTable data={this.state.times} striped hover>
+				    <TableHeaderColumn isKey dataField='id'>Id</TableHeaderColumn>
+				    <TableHeaderColumn dataField='start'>Start</TableHeaderColumn>
+				    <TableHeaderColumn dataField='end'>End</TableHeaderColumn>
+				    <TableHeaderColumn dataField='title'>Title</TableHeaderColumn>
+				    <TableHeaderColumn dataField='category'>Category</TableHeaderColumn>
+				</BootstrapTable>
+			    </Row>
+			</div>
+		    </Col>
+		</Row>
+	    </div>
     )}
 }
 
